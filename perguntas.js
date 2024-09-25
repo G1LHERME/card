8d2e2bf66b60389912257592c2174f4fb3449f65
G1LHERME <161774624+G1LHERME@users.noreply.github.com>
2024-09-25 19:47:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Geografia ', 
+    'Qual a capital do brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital do paraná? ',
+    'Curitiba '
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da frança?',
+    'Paris '
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da alemanha?',
+    'Berlim'
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da itália?',
+    'Roma '
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da espanha?',
+    'Madrid '
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital de portugal? ',
+    'Lisboa '
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da polõnia?',
+    'Varsóvia '
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da ucrãnia?',
+    'Kiev'
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a capital da rússia?',
+    'Moscou'
+)
